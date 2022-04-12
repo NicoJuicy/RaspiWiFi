NicoJuicy/RaspiWiFi

commit b2a43787478bcb2bde5e51ac2e09765429a5ea2a

configuration_app:advanced: Require SSH password for SSH enabled
If SSH password is not given or not alphanumerica, SSH is disabled.

libs/configuration_app/app.py
@@ -32,16 +32,20 @@ def save_credentials():
     ssid = request.form['ssid']
     wifi_key = request.form['wifi_key']
     ssh_enabled = request.form['ssh_enabled'] if 'ssh_enabled' in request.form else False
+    ssh_password = request.form['ssh_password'] if 'ssh_password' in request.form else None
+
+    if not ssh_enabled:
+        ssh_password = None
 
     create_wpa_supplicant(ssid, wifi_key)
 
     # Call set_ap_client_mode() in a thread otherwise the reboot will prevent
     # the response from getting to the browser
-    def sleep_and_start_ap(ssh_enabled):
+    def sleep_and_start_ap(ssh_password):
         time.sleep(2)
-        set_ssh(ssh_enabled)
+        set_ssh(ssh_password)
         set_ap_client_mode()
-    t = Thread(target=sleep_and_start_ap, args=(ssh_enabled, ))
+    t = Thread(target=sleep_and_start_ap, args=(ssh_password,))
     t.start()
 
     return render_template('save_credentials.html', ssid = ssid)
@@ -107,9 +111,16 @@ def create_wpa_supplicant(ssid, wifi_key):
 
     os.system('mv wpa_supplicant.conf.tmp /etc/wpa_supplicant/wpa_supplicant.conf')
 
-def set_ssh(enabled: bool):
-    if enabled:
+def set_ssh(password=None):
+    if password:
+        if ':' in password or '"' in password:
+            password = None
+        if not password.isalpha():
+            password = None
+
+    if password:
         os.system('touch /boot/ssh')
+        os.system(f'echo "pi:{password}" | chpasswd')
     else:
         os.system('/etc/init.d/ssh stop')
         os.system('update-rc.d ssh disable')

libs/configuration_app/static/stylesheets/app.css
@@ -21,6 +21,19 @@ body {
   background-color: #f5f5f5;
 }
 
+details {
+  padding: 1em. 1em 0;
+}
+
+details[open] {
+  padding: .5em;
+}
+
+summary {
+  margin: -.5em -.5em 0;
+  padding: .5em;
+}
+
 #mainContent {
   width: 100%;
   max-width: 330px;

libs/configuration_app/templates/app.html
@@ -20,10 +20,17 @@
         <label for="wifi_key">Your Wifi password</label>
         <input type="password" name="wifi_key", class="wifiNetworkInputs form-control">
       </div>
-      <div class="form-group">
-        <label for="ssh_enabled">SSH enabled</label>
-        <input type="checkbox" name="ssh_enabled" class="form-control">
-      </div>
+      <details><summary>Advanced (optional)</summary>
+        <div class="form-group">
+          <label for="ssh_enabled">SSH enabled</label>
+          <input type="checkbox" name="ssh_enabled" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="ssh_password">SSH password</label>
+          <input type="password" name="ssh_password" class="form-control">
+          <span>Alphanumerical characters only. If you leave it empty, <b>SSH will be disabled</b>.</span>
+        </div>
+      </details>
       <button type="submit", class="wifiConnectButton btn btn-lg btn-success btn-block">Submit</button>
     </form>
 

libs/configuration_app/templates/manual_ssid_entry.html
@@ -2,7 +2,7 @@
 
 {% block body %}
   <div id="mainContent">
-    
+
     <a href="/"><img class="mb-4" src="{{ url_for('static', filename='one-meter.svg') }}" alt="" width="125" height="125"></a>
     <h1>WiFi Setup</h1>
 
@@ -15,10 +15,17 @@
         <label for="wifi_key">Wifi password</label>
         <input type="password" name="wifi_key", class="wifiNetworkInputs form-control">
       </div>
-      <div class="form-group">
-        <label for="ssh_enabled">SSH enabled</label>
-        <input type="checkbox" name="ssh_enabled" class="form-control">
-      </div>
+      <details><summary>Advanced (optional)</summary>
+        <div class="form-group">
+          <label for="ssh_enabled">SSH enabled</label>
+          <input type="checkbox" name="ssh_enabled" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="ssh_password">SSH password</label>
+          <input type="password" name="ssh_password" class="form-control">
+          <span>Alphanumerical characters only. If you leave it empty, <b>SSH will be disabled</b>.</span>
+        </div>
+      </details>
       <button type="submit" class="wifiConnectButton btn btn-lg btn-success btn-block">Submit</button>
     </form>
 

commit 983fd4ab9e9fb6e093a4746c74f9b0e07fe1e255

fix:configuration_app:advance: Require alnum password
Use isalnum instead of isalpha.

https://docs.python.org/3/library/stdtypes.html#str.isalnum
https://docs.python.org/3/library/stdtypes.html#str.isalpha

Fixes: b2a43787478bcb2bde5e51ac2e09765429a5ea2a

libs/configuration_app/app.py
@@ -115,7 +115,7 @@ def set_ssh(password=None):
     if password:
         if ':' in password or '"' in password:
             password = None
-        if not password.isalpha():
+        if not password.isalnum():
             password = None
 
     if password: